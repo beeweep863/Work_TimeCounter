40dee039b7657fde10a42a93a62fe0190ee20356
luiss_benvenuto <97201998+beeweep863@users.noreply.github.com>
2024-06-21 09:43:43 -0600

    pomTime update

---
+++ Work_TimeCounter/Logic/HoursCounter.swift
@@ -0,0 +1,18 @@
+//
+//  HoursCounter.swift
+//  Work_TimeCounter
+//
+//  Created by Luis Benvenuto on 21/06/24.
+//
+
+import Foundation
+
+class HoursCounter{
+    
+    func addHour(){
+        
+    }
+    func removeHour(){
+        
+    }
+}
